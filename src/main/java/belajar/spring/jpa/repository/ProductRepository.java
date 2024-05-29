faeee1af108a4a9f70cfa04091498cf6de33bea6
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-29 13:26:00 +0700

    paging

--- src/main/java/belajar/spring/jpa/repository/ProductRepository.java
+++ src/main/java/belajar/spring/jpa/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package belajar.spring.jpa.repository;
 
 import belajar.spring.jpa.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByCategory_Name(String name);
 
     List<Product> findAllByCategory_Name(String name, Sort sort);
+
+    Page<Product> findAllByCategory_Name(String name, Pageable pageable);
 }
--- src/test/java/belajar/spring/jpa/repository/ProductRepositoryTest.java
+++ src/test/java/belajar/spring/jpa/repository/ProductRepositoryTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.support.TransactionOperations;
 
@@ -60,5 +63,25 @@ class ProductRepositoryTest {
         assertEquals(2, products.size());
         assertEquals("Adidas", products.get(0).getName());
         assertEquals("Nike", products.get(1).getName());
+    }
+
+    @Test
+    void findByCategoryNameSortAndPaging() {
+//        page 0
+        PageRequest pageRequest = PageRequest.of(0, 1, Sort.by(Sort.Order.desc("id")));
+        Page<Product> products = productRepository.findAllByCategory_Name("SHOES MURAH", pageRequest);
+        assertEquals(1, products.getContent().size());
+        assertEquals(0, products.getNumber());
+        assertEquals(2, products.getTotalElements());
+        assertEquals(2, products.getTotalPages());
+        assertEquals("Adidas", products.getContent().get(0).getName());
+//        page 1
+        pageRequest = PageRequest.of(1, 1, Sort.by(Sort.Order.desc("id")));
+        products = productRepository.findAllByCategory_Name("SHOES MURAH", pageRequest);
+        assertEquals(1, products.getContent().size());
+        assertEquals(1, products.getNumber());
+        assertEquals(2, products.getTotalElements());
+        assertEquals(2, products.getTotalPages());
+        assertEquals("Nike", products.getContent().get(0).getName());
     }
 }
